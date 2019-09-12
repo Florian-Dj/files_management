1f5f0c4297161f0a2ea6d13de409d50ae885aba4
Mucral <43535702+Mucral@users.noreply.github.com>
2019-09-13 01:48:31 +0200

    Projet fonctionnel
    Gérer des fichiers dans un dossier général, en les déplaçant vers un dossier du même nom.

---
+++ file_sorter.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shutil
+from datetime import datetime
+import missing_files
+
+path_folder = "D:\\DL\\Animes"  # Chemin vers le dossier a traité
+list_folder = []  # Liste des dossiers
+list_file = []  # Liste des fichiers sans dossiers
+logs_list = []  # Liste des logs
+check_folder = ["Vrac", "Films", "Doublon"]  # Liste des dossiers obligatoire
+
+
+def folders_list():  # Liste des dossiers
+    for folder in next(os.walk(path_folder))[1]:  # Check tous les dossiers
+        if folder not in check_folder:  # Si le dossier check n'est pas dans la liste check_folder
+            list_folder.append(folder)  # Ajout du dossier dans la liste list_folder
+        else:
+            check_folder.remove(folder)  # Supprime le dossier si existant de la liste check_folder
+    for folders in check_folder:
+        os.mkdir("{p}\\{f}".format(p=path_folder, f=folders))  # Création dossiers qui reste dans la liste check_folder
+        logs_add(folders, folders, folders, "NEWS")
+    return list_folder
+
+
+def files_list():  # Liste des fichiers en .mp4 et .mkv
+    for files in os.listdir(path_folder):
+        _, ext = os.path.splitext(files)
+        if ext == ".mp4" or ext == ".mkv" or ext == ".MP4":
+            list_file.append(files)
+    return list_file
+
+
+def compare(folder_list, file_list):  # Comparaison des dossiers et des fichiers
+    for file in file_list:  # Boucle des fichiers
+        no_folder = 0
+        for folder in folder_list:  # Boucle des dossiers
+            if file.split('.')[0] == folder.split(" ")[0] or \
+                file.split("_")[0] == folder.split(" ")[0] or \
+                file.split(" ")[0] == folder.split(" ")[0] or \
+                    file.split("-")[0] == folder.split(" ")[0]:  # Premier mot file == premier mot folder
+                move_rename(file, folder)
+            elif no_folder + 1 == len(folder_list):
+                text_warning(file)
+            else:
+                no_folder += 1
+    # logs_write(logs_list)
+
+
+def text_warning(file):
+    print("""====== Fichier sans dossier ======
+    {}
+    1 - Voir les dossiers existant
+    2 - Déplacer dans le dossier Vrac
+    3 - Déplacer dans le dossier Films
+    4 - Créer un dossier
+    5 - Pas actions
+    """.format(file))
+    warning(file)
+
+
+def warning(file):
+    choose = input("Merci de faire votre choix :")  # Input pour faire son choix d'action
+    choose_list = ["1", "2", "3", "4", "5"]  # Liste des choix possible
+    if choose in choose_list:
+        if choose == "1":
+            text_choose_folder(file)
+        elif choose == "2":  # On Déplace le fichier dans le dossier Vrac
+            move(file, "Vrac")
+        elif choose == "3":  # On déplace le fichier dans le dossier Films
+            move(file, "Films")
+        elif choose == "4":
+            create_folder(file)
+        elif choose == "5":
+            pass
+    else:
+        print("Faire une bonne saisie!")
+        warning(file)
+
+
+def text_choose_folder(file):
+    # Print pour voir tous les dossiers existant
+    print("""====== Fichier sans dossier ======
+    {}""".format(file))
+    n = 1
+    for folder in list_folder:
+        print("    {n} - {f}".format(n=n, f=folder))
+        n += 1
+    print("    0 - Retour")
+    choose_folder(file)
+
+
+def choose_folder(file):
+    choose = int(input("Dans quel dossier voulez-vous transférer le fichier ?"))
+    if choose == 0:
+        text_warning(file)
+    elif 1 <= choose <= len(list_folder):
+        move_rename(file, list_folder[choose - 1])
+    else:
+        print("Mauvaise saisie !")
+        choose_folder(file)
+
+
+def move(file, folder):
+    if folder == "Vrac":
+        logs_add(folder, file, file, "WARNING")  # Lance la function pour les logs
+    else:
+        logs_add(folder, file, file, "INFO")  # Lance la function pour les logs
+    old_file = "{p}\\{a}".format(p=path_folder, a=file)  # Chemin de l'ancien fichier
+    if duplicate(file, folder, old_file):
+        new_file = "{p}\\{f}\\{a}.mp4".format(p=path_folder, f=folder, a=file)  # Chemin du nouveau fichier
+        shutil.move(old_file, new_file)  # Déplace le fichier dans le dossier
+
+
+def move_rename(file, folder):  # Déplacer et Renommer un fichier
+    try:  # Si le fichier a un numéro
+        # Trouve le numéro dans le nom du fichier
+        if re.findall(r"Épisode \d+", file):
+            number = re.findall(r"Épisode \d+", file)[0][8:]
+        elif re.findall(r"S\d+\.\d+", file):
+            number = re.findall(r"S\d+\.\d+", file)[0].split('.')[1]
+        elif re.findall(r"E\d+", file):
+            number = re.findall(r"E\d+", file)[0].split('E')[1]
+        else:
+            number = re.findall(r'\d+', file)[0]
+        number_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        if number in number_list:
+            number = "0" + number
+        new_name = "{f} {n}.mp4".format(f=folder, n=number)  # Nom du nouveau fichier
+    except IndexError:  # Si fichier non numéro
+        print("Dans le except")
+        new_name = "{f}.mp4".format(f=folder)  # Nom du nouveau fichier
+    old_file = "{p}\\{a}".format(p=path_folder, a=file)  # Chemin de l'ancien fichier
+    if duplicate(new_name, folder, old_file):
+        logs_add(folder, file, new_name, "INFO")  # Lance la function pour les logs
+        new_file = "{p}\\{f}\\{n}".format(p=path_folder, f=folder, n=new_name)  # Chemin du nouveau fichier
+        shutil.move(old_file, new_file)  # Déplace le fichier dans le dossier
+
+
+def create_folder(file):  # Création de dossier
+    new_folder = input("Nom du nouveau dossier :")
+    if new_folder in list_folder:
+        print("Un dossier du même nom existe déjà")
+        create_folder(file)
+    else:
+        os.mkdir("{p}\\{n}".format(p=path_folder, n=new_folder))
+        logs_add(new_folder, file, file, "NEWS")
+        move_rename(file, new_folder)
+
+
+def duplicate(file, folder, old_file):
+    name = old_file.split("\\")[3]
+    list_files = "{p}\\{f}".format(p=path_folder, f=folder)
+    check = True
+    for files in os.listdir(list_files):
+        if file == files:
+            logs_add("Doublon", name, name, "DUPLICATE")  # Lance la function pour les logs
+            new_file = "{p}\\{f}".format(p=path_folder, f="Doublon")  # Chemin du nouveau fichier
+            shutil.move(old_file, new_file)  # Déplace le fichier dans le dossier
+            check = False
+    if check:
+        return check
+
+# Toutes les def pour les Logs
+
+
+def logs_read():  # Lire le fichier des logs
+    if "logs.txt" in os.listdir(path_folder):
+        with open("{p}\\logs.txt".format(p=path_folder), mode="r", encoding='utf-8') as text:
+            logs_list.append(text.read())
+    else:
+        with open("{p}\\logs.txt".format(p=path_folder), mode="w", encoding='utf-8') as text:
+            text.write("{d}[NEWS]: Création de logs.txt".format(d=datetime.now().strftime("[%d-%m-%Y - %H:%M:%S]")))
+    return logs_list
+
+
+def logs_add(folder, old_file, new_file, info):  # Ajouter les logs dans la liste log
+    if info == "NEWS":  # Ecriture dans le fichier logs
+        logs_list.insert(0, "{d}[{i}]: Création du dossier {f}\n".format(d=datetime.now().strftime("\
+[%d-%m-%Y - %H:%M:%S]"), i=info, f=folder))
+    else:  # Ecriture dans le fichier logs
+        logs_list.insert(0, "{d}[{i}]: {o} ==> {f}\\{n}\n".format(d=datetime.now().strftime("\
+[%d-%m-%Y - %H:%M:%S]"), i=info, o=old_file, f=folder, n=new_file))
+    return logs_list
+
+
+"""
+def logs_write(logs):  # Ecriture les logs dans le fichier
+    with open("{p}\\logs.txt".format(p=path_folder), mode="w", encoding='utf-8') as text:
+        for logs_list_write in logs:  # Boucle pour lire toute la liste logs
+            text.write(logs_list_write)  # Ecriture dans le fichier logs
+"""
+
+
+if __name__ == "__main__":
+    logs_read()
+    compare(folders_list(), files_list())
+    missing_files.compare(list_folder, path_folder, logs_list)
+
+
+"""
+    Problème si deux dossiers commence avec le même nom (One Peace, One Punch Man) [One]
+"""
---
+++ missing_files.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import re
+import os
+from datetime import datetime
+
+list_files = []  # Liste des fichiers sans dossiers
+missing_files = []  # Liste des fichiers manquant
+
+
+def compare(list_folder, path_folder, logs_list):
+    for folders in list_folder:
+        # Liste avec nom dossier + nom fichiers eux même dans une liste
+        list_files.append([folders] + [os.listdir("{p}\\{f}".format(p=path_folder, f=folders))])
+    i = 0  # Variable i pour incrémenter dossiers
+    while i < len(list_files):
+        o = 0  # Variable o pour incrémenter fichiers
+        while o < len(list_files[i][1]):
+            _, ext = os.path.splitext(list_files[i][1][o])  # Inspecte l'extension du fichier
+            if not ext == ".mp4" or ext == ".mkv" or ext == ".MP4":
+                list_files[i][1].remove(list_files[i][1][o])  # Sup les fichiers sans la bonne extension de la liste
+            else:
+                o += 1
+        maximum = int(re.findall(r" \d+", list_files[i][1][-1])[0])  # Look le numero max des fichiers dans le dossier
+        minimum = int(re.findall(r" \d+", list_files[i][1][0])[0])  # Look le numero min des fichiers dans le dossier
+        if len(list_files[i][1]) < maximum - minimum + 1:  # Si moins de fichier que le calcul max - min + 1
+            x = 1  # Incémente le numéro du fichier par le nom
+            y = 0  # Incémente le numéro du fichier dans la liste
+            missing_number = []  # Reset de la liste des numéros manquant
+            while y < len(list_files[i][1]):
+                # Si numéro du fichier dans la liste == numéro du fichier par la nom
+                if int(re.findall(r" \d+", list_files[i][1][y])[0].split(" ")[1]) == x:
+                    x += 1
+                    y += 1
+                else:
+                    x += 1
+                    missing_number.append(x)  # Ajout du numéro du fichier manquant dans la liste missing_list
+            # Ajout du nom du dossier et de la missing_list dans une liste
+            missing_files.append([list_files[i][0], missing_number])
+        i += 1
+    for logs in missing_files:
+        log(logs, path_folder, logs_list)  # Lancement de la function logs
+
+
+def log(logs, path_folder, logs_list):
+    logs_list.insert(0, "{d}[{i}]: {f} il manque les fichiers {n}\n".format(d=datetime.now().strftime("\
+[%d-%m-%Y - %H:%M:%S]"), i="MISSING", f=logs[0], n=logs[1]))  # Insertion du texte logs dans la liste logs
+    with open("{p}\\logs.txt".format(p=path_folder), mode="w", encoding='utf-8') as text:
+        for logs_list_write in logs_list:  # Boucle pour lire toute la liste logs
+            text.write(logs_list_write)  # Ecriture dans le fichier logs
